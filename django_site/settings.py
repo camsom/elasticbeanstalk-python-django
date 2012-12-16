d2438d3107a8789f7008b09c5fa7d40ccab07acd
yuta imai <imai.factory@gmail.com>
2012-12-17 00:09:53 +0900

    db

--- django_site/settings.py
+++ django_site/settings.py
@@ -1,5 +1,7 @@
 # Django settings for django_site project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,12 +13,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.environ['RDS_DB_NAME'],                      # Or path to database file if using sqlite3.
+        'USER': os.environ['RDS_USERNAME'],                      # Not used with sqlite3.
+        'PASSWORD': os.environ['RDS_PASSWORD'],                  # Not used with sqlite3.
+        'HOST': os.environ['RDS_HOSTNAME'],                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.environ['RDS_PORT'],                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
